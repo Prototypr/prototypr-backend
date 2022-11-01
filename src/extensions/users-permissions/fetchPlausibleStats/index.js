f4be090179fd58ba7d269651a9a2cf826ab43df3
Joel Benjamin <joelbenjamin093@gmail.com>
2022-11-01 17:11:41 +0530

    code cleanup

--- src/extensions/users-permissions/fetchPlausibleStats/index.js
+++ src/extensions/users-permissions/fetchPlausibleStats/index.js
@@ -8,67 +8,14 @@ const fs = require("fs");
 
 const _ = require("lodash");
 const utils = require("@strapi/utils");
-// const { getService } = require("./libs/getService");
-// const { validateUpdateUserBody } = require("./libs/userValidation");
+
+const { fetchTimeSeriesData, fetchPlausibleData } = require("./utils");
 
 const { sanitize } = utils;
 
 const axios = require("axios");
-
-const plausibleKey =
-  "XjrEoqLmHJhq6A1KLybxBHD2bM6ou1ZYK1REKk7h9tjDc4wZ3aqXwPaWVROE8dk_";
-
+const plausibleKey = process.env.PLAUSIBLE_KEY;
 const baseURL = "https://analytics.prototypr.io/api/v1";
-
-const fetchPlausibleData = async (slug, metrics = ["pageviews"]) => {
-  const metricsString = metrics.join(",");
-  // visits,bounce_rate,visit_duration,pageviews
-  const apiEndpoint = `${baseURL}/stats/aggregate`;
-
-  const siteId = "4.prototypr.io";
-  const todayDate = new Date().toISOString().slice(0, 10);
-
-  const plausibleRequest = `${apiEndpoint}?site_id=${siteId}&filters=event:page%3D%3D%2Fpost/${slug}&metrics=${metricsString}&period=custom&date=2021-01-01,${todayDate}`;
-
-  let configUpload = {
-    method: "get",
-    url: plausibleRequest,
-    headers: {
-      Authorization: `Bearer ${plausibleKey}`,
-    },
-    data: {},
-  };
-
-  const response = await axios(configUpload).catch(function (error) {
-    console.log(error);
-  });
-
-  return response.data.results;
-};
-
-// fetch time series data
-const fetchTimeSeriesData = async (slug, metrics = ["pageviews"]) => {
-  const metricsString = metrics.join(",");
-  const apiEndpoint = `${baseURL}/stats/timeseries`;
-  const siteId = "4.prototypr.io";
-
-  // https://analytics.prototypr.io/api/v1/stats/timeseries?site_id=4.prototypr.io&metrics=visits,bounce_rate,visit_duration,pageviews&period=30d&filters=event:page%3D%3D%2Fpost/are-ui-ux-tips-the-new-clickbait-for-designerse29ca8
-  const plausibleRequest = `${apiEndpoint}?site_id=${siteId}&metrics=${metricsString}&period=30d&filters=event:page%3D%3D%2Fpost/${slug}`;
-
-  let configUpload = {
-    method: "get",
-    url: plausibleRequest,
-    headers: {
-      Authorization: `Bearer ${plausibleKey}`,
-    },
-  };
-
-  const response = await axios(configUpload).catch(function (error) {
-    console.log(error);
-  });
-
-  return response.data.results;
-};
 
 module.exports = {
   /**
@@ -98,7 +45,6 @@ module.exports = {
     if (user) {
       const { posts: userPosts } = user;
       const slugExistsInPost = userPosts.find((post) => post.slug === slug);
-      console.log("slugExits -", slugExistsInPost);
       if (slugExistsInPost) {
         const s = slug; // "take-me-to-the-useless-webs";
         // fetch plausible data
@@ -125,12 +71,7 @@ module.exports = {
         ctx.send({ status: 401, msg: "Access Denied!" });
       }
     } else {
-      console.log("user not found");
       ctx.send({ status: 401, msg: "Access Denied!" });
     }
-
-    console.log("fetching stats for user");
-
-    // ctx.send({ status: true });
   },
 };
