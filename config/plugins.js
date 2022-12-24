58a79179aa6b1d097c456816ce4cd58d9715476c
Graeme Fulton <graemefulton89@gmail.com>
2022-12-24 19:49:40 +0000

    sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -41,10 +41,14 @@ module.exports = ({ env }) => ({
     email: {
       config: {
         provider: 'mailgun',
+        // providerOptions: {
+        //   apiKey: env('MAILGUN_API_KEY'),
+        //   domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
+        //   host: env('MAILGUN_HOST', 'api.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+        // },
+        provider: 'sendgrid',
         providerOptions: {
-          apiKey: env('MAILGUN_API_KEY'),
-          domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
-          host: env('MAILGUN_HOST', 'api.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+          apiKey: env('SENDGRID_API_KEY'),
         },
         settings: {
           defaultFrom: 'hello@prototypr.io',
