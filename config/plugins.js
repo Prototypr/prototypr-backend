2b0db124e06eb7169df95c737aed9b142e5eefdc
Kems <rufai@hotmail.co.uk>
2023-01-11 01:23:52 +0000

    multiple images added

--- config/plugins.js
+++ config/plugins.js
@@ -41,14 +41,10 @@ module.exports = ({ env }) => ({
     email: {
       config: {
         provider: 'mailgun',
-        // providerOptions: {
-        //   apiKey: env('MAILGUN_API_KEY'),
-        //   domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
-        //   host: env('MAILGUN_HOST', 'api.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
-        // },
-        provider: 'sendgrid',
         providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
+          apiKey: env('MAILGUN_API_KEY'),
+          domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
+          host: env('MAILGUN_HOST', 'api.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
         },
         settings: {
           defaultFrom: 'hello@prototypr.io',
